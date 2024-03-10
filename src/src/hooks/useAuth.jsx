9163cd3d4a02308ca79b8fc18f7ac835d3b83bdd
developedbyrayhan <islamraihanofficial@gmail.com>
2024-03-10 11:54:21 +0600

    fix: src dir issues

--- src/src/hooks/useAuth.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "../providers/AuthProvider";
-
-const useAuth = () => {
-    const all = useContext(AuthContext);
-    return all
-};
-
-export default useAuth;
